e11f82d3cad8147cac00bff71b2f48d99b82cfd5
rsuneja <rajatsuneja1@gmail.com>
2016-05-23 10:39:38 +0530

    New Files

---
+++ script.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const _ = require('lodash');
+const Script = require('smooch-bot').Script;
+
+const scriptRules = require('./script.json');
+
+module.exports = new Script({
+    processing: {
+        //prompt: (bot) => bot.say('Beep boop...'),
+        receive: () => 'processing'
+    },
+
+    start: {
+        receive: (bot) => {
+            return bot.say('So you want to get to know me?')
+                .then(() => 'speak');
+        }
+    },
+
+    speak: {
+        receive: (bot, message) => {
+
+            let upperText = message.text.trim().toUpperCase();
+
+            function updateSilent() {
+                switch (upperText) {
+                    case "CONNECT ME":
+                        return bot.setProp("silent", true);
+                    case "DISCONNECT":
+                        return bot.setProp("silent", false);
+                    default:
+                        return Promise.resolve();
+                }
+            }
+
+            function getSilent() {
+                return bot.getProp("silent");
+            }
+
+            function processMessage(isSilent) {
+                if (isSilent) {
+                    return Promise.resolve("speak");
+                }
+
+                if (!_.has(scriptRules, upperText)) {
+                    return bot.say(`Maalik ne abhi itna ni sikhaya!!`).then(() => 'speak');
+                }
+
+                var response = scriptRules[upperText];
+                var lines = response.split(/(<img src=\'[^>]*\'\/>)/);
+
+                var p = Promise.resolve();
+                _.each(lines, function(line) {
+                    line = line.trim();
+                    if (!line.startsWith("<")) {
+                        p = p.then(function() {
+                            return bot.say(line);
+                        });
+                    } else {
+                        // p = p.then(function() {
+                        //     var start = line.indexOf("'") + 1;
+                        //     var end = line.lastIndexOf("'");
+                        //     var imageFile = line.substring(start, end);
+                        //     return bot.sendImage(imageFile);
+                        // });
+                    }
+                })
+
+                return p.then(() => 'speak');
+            }
+
+            return updateSilent()
+                .then(getSilent)
+                .then(processMessage);
+        }
+    }
+});
